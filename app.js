e858a7fb82077e4c1e35a1a471b45c7cc47c2b89
ibotdotout <tkroputa@gmail.com>
2017-06-22 20:03:34 +0700

    middleware:
    https://expressjs.com/en/guide/using-middleware.html

--- app.js
+++ app.js
@@ -17,6 +17,22 @@ app.delete('/user', function (req, res) {
     res.send('Got a DELETE request at /user')
 })
 
+const cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+const cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+const cb2 = function (req, res) {
+    res.send('Hello from C!')
+}
+
+app.get('/middlewares', [cb0, cb1, cb2])
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
