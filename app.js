fd2c6e21b7d198f738934bce9fb098708000597e
ibotdotout <tkroputa@gmail.com>
2017-06-22 21:26:25 +0700

    refactor: middleware to controller

--- app.js
+++ app.js
@@ -5,22 +5,6 @@ const router = require(path.resolve('app/routes/router'))
 
 app.use('/', router)
 
-const cb0 = function (req, res, next) {
-    console.log('CB0')
-    next()
-}
-
-const cb1 = function (req, res, next) {
-    console.log('CB1')
-    next()
-}
-
-const cb2 = function (req, res) {
-    res.send('Hello from C!')
-}
-
-app.get('/middlewares', [cb0, cb1, cb2])
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
--- app/routes/router.js
+++ app/routes/router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const basic = require('../controllers/basic')
+const middleware = require('../controllers/middleware')
 
 router.route('/')
   .get(basic.get)
@@ -10,5 +11,8 @@ router.route('/')
 router.route('/user')
   .put(basic.put)
   .delete(basic.del)
+
+router.route('/middleware')
+  .get(middleware)
 
 module.exports = router
